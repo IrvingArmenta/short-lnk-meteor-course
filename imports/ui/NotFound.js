77ab28b4d4316e966d3a69f3efd11ca75e15384b
Irving Armenta <irving@irvingarmenta.com>
2017-07-24 13:23:10 -0600

    Updated NotFound.js text

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
     return (
       <div className="boxed-view">
         <div className="boxed-view__box">
-          <h1>Error 404! : (</h1>
+          <h1>Error 404!</h1>
           <h4>Page not found</h4>
           <Link className="button button--link" to="/">Head Home</Link>
         </div>
